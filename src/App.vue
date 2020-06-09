95b40017aa78ad6c9b67eef71a014f2c57f34075
djhoon <742471208@qq.com>
2020-06-09 18:39:39 +0800

    zzz

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,8 @@
 <template>
-<<<<<<< HEAD
   <div id="app">Welcome
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
-=======
-  <div id="app">
-    <router-view></router-view>
-  </div>  
->>>>>>> 2bac135da3280078303bbb547b18d64e0fd14d3d
 </template>
 
 <script>  
