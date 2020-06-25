bbb5921925d467cc23dab8c4a6ca7bf1be175d7b
djhoon <742471208@qq.com>
2020-06-25 20:18:14 +0800

    nocomponents

--- src/main.js
+++ src/main.js
@@ -9,8 +9,5 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
-  render: function (h) { return h(App) },
-  components: {
-
-  }
+  render: function (h) { return h(App) }
 }).$mount('#app')
